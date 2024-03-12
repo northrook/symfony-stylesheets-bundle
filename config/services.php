6590c95384078baecfae674a8c26fcbf9d78cc95
Martin <hello@northrook.com>
2024-03-12 10:13:30 +0000

    mend

--- config/services.php
+++ config/services.php
@@ -2,37 +2,34 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Northrook\Symfony\Core\Support\Path;
 use Northrook\Symfony\Stylesheets\Services\StylesheetGenerationService;
 
 return static function ( ContainerConfigurator $container ) : void {
 
-	$fromRoot = static fn ( string $set = '' ) => $set ? trim(
-		'%kernel.project_dir%' . DIRECTORY_SEPARATOR . trim(
-			str_replace( [ '\\', '/' ], DIRECTORY_SEPARATOR, $set ), DIRECTORY_SEPARATOR,
-		) . DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR,
-	) : $set;
+    // Parameters
+    $container->parameters()
+              ->set( 'dir.root', Path::from( '%kernel.project_dir%', ) )
+              ->set( 'dir.stylesheets.output', Path::from( '%kernel.project_dir%/public/assets/styles' ) )
+    ;
 
-	// Parameters
-	$container->parameters()
-	          ->set( 'dir.root', $fromRoot() )
-	          ->set( 'dir.stylesheets.output', $fromRoot( "/public/assets/styles" ) )
-	;
-
-	// Services
-	$container->services()
-		//
-		//
-		// 🗃️️ - Stylesheet Generation Service
-		      ->set( 'stylesheet.service.generator', StylesheetGenerationService::class )
-	          ->args( [
-		                  service( 'parameter_bag' ),
-		                  service( 'session' )->nullOnInvalid(),
-		                  service( 'logger' )->nullOnInvalid(),
-		                  service( 'debug.stopwatch' )->nullOnInvalid(),
-	                  ] )
-	          ->autowire()
-	          ->alias( StylesheetGenerationService::class, 'stylesheet.service.generator' )
-	;
+    // Services
+    $container->services()
+        //
+        //
+        // 🗃️️ - Stylesheet Generation Service
+              ->set( 'stylesheet.service.generator', StylesheetGenerationService::class )
+              ->args(
+                  [
+                      service( 'parameter_bag' ),
+                      service( 'session' )->nullOnInvalid(),
+                      service( 'logger' )->nullOnInvalid(),
+                      service( 'debug.stopwatch' )->nullOnInvalid(),
+                  ],
+              )
+              ->autowire()
+              ->alias( StylesheetGenerationService::class, 'stylesheet.service.generator' )
+    ;
 
 
 };
